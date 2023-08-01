0cfce5354bcc2e0bd393a13bd21d260939027e00
Yeminfu <yeminfu4319@gmail.com>
2023-08-02 08:48:03 +1000

    rm cache on get tasks route

--- src/app/api/tasks/get/route.ts
+++ src/app/api/tasks/get/route.ts
@@ -1,8 +1,11 @@
 import { getTasks } from '@/app/page';
-import { NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
     const tasks = await getTasks();
+    const path = request.nextUrl.searchParams.get('path');
+    if (path) revalidatePath(path);
     return NextResponse.json({
         success: true,
         tasks
