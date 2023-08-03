ffc9988d2cd06422581f365fbc382f68cf63c67f
Yeminfu <yeminfu4319@gmail.com>
2023-08-03 14:06:00 +1000

    create create project route

---
+++ src/app/api/projects/create/route.ts
@@ -0,0 +1,38 @@
+import db_connection from '@/app/tools/dbConnect';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+    const project = await request.json();
+
+    if (!project.title) {
+        return NextResponse.json({
+            success: false,
+            err: "#dm32mcs09"
+        });
+    }
+
+    // const res = await new Promise(r => {
+    //     const qs = `UPDATE tasks SET title = ?, column_id = ? WHERE id = ?`;
+    //     const values = [task.title, task.column_id, task.id];
+    //     console.log({ qs, values });
+
+    //     db_connection.query(qs, values, function (err, res: any) {
+    //         if (err) { console.log('err #c9c6f3bnNn', err) }
+    //         console.log(res);
+
+    //         r(res.affectedRows);
+    //     })
+    // });
+
+    // if (res) {
+    return NextResponse.json({
+        success: true,
+        project,
+    });
+    // } else {
+    //     return NextResponse.json({
+    //         success: false,
+    //     });
+    // }
+
+}
